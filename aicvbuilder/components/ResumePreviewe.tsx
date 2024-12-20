113d8a7827e3fa7487b47adb562779d6ce92d73e
ramzijebalia <jebaliaramzi@gmail.com>
2024-12-20 20:49:37 +0100

    display the education section in the cv

--- aicvbuilder/components/ResumePreviewe.tsx
+++ aicvbuilder/components/ResumePreviewe.tsx
@@ -29,6 +29,7 @@ export default function ResumePreview({resumeData , clasName}: ResumePreviewProp
                 <PersonalInfoHeader resumeData={resumeData}/>
                 <SummarySection resumeData={resumeData}/>
                 <WorkExperienceSection resumeData={resumeData}/>
+                <EducationSection resumeData={resumeData}/>
             </div>
         </div>
 
@@ -130,6 +131,41 @@ function WorkExperienceSection({resumeData} : ResumePreviewSectionProps){
                         <div className="whitespace-pre-line text-sm">
                             {exp.description}
                         </div>
+                    </div>
+                ))}
+            </div>
+        </>
+        
+)}
+
+function EducationSection({resumeData} : ResumePreviewSectionProps){
+    const {educations} = resumeData
+    const educationsNotEmpty = educations?.filter(
+        (edu) => Object.values(edu).filter(Boolean).length > 0
+    )
+
+    if(!educationsNotEmpty?.length) return null
+
+
+    return(
+        <>
+            <hr className="border-2" />
+            <div className="text-y-3">
+                <p className="text-lg font-semibold">Education</p>
+                {educationsNotEmpty.map((edu , index) => (
+                    <div key={index} className="break-inside-avoid space-y-1 m-3">
+                        <div className="flex items-center justify-between text-sm font-semibold">
+                            <span>{edu.degree}</span>
+                            {edu.startDate &&  (
+                                <span>
+                                    {edu.startDate &&
+                                    `${formatDate(edu.startDate , "MM/yyyy")} ${edu.endDate ? `- ${formatDate(edu.endDate , "MM/yyyy")}`: ""}`
+                                    
+                                    }
+                                </span>
+                            )}
+                        </div>
+                        <p className="text-xs font-semibold">{edu.school}</p>
                     </div>
                 ))}
             </div>
